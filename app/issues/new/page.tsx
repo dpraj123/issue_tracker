1bdd32818946b12d1029f67561cdf7e965a297ab
dpraj123 <rajdp764@gmail.com>
2024-06-18 03:03:11 +0530

    show issue list

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,44 +1,48 @@
 "use client";
+import React, { useState } from "react";
+import { z } from "zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import React from "react";
 import { Controller, useForm } from "react-hook-form";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const {
     register,
     handleSubmit,
     control,
     formState: { errors },
-  } = useForm<IssueForm>();
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
-  const handleNewIssueForm = (values: IssueForm) => {
-    console.log(values);
+  const handleNewIssueForm = async (FormValue: IssueForm) => {
+    try {
+      setIsSubmitting(true);
+      await axios.post("/api/issues", FormValue);
+      router.push("/issues");
+    } catch (error) {
+      console.error("Error submitting form: ", error);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
-
   return (
-    <form
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
+    <form onSubmit={handleSubmit(handleNewIssueForm)}>
       <div className="max-w-xl space-y-4 p-5">
         <TextField.Root
           placeholder="Title"
           {...register("title", { required: "Title is required" })}
         />
-        {errors.title && (
-          <div className="text-red-500 text-sm">{errors.title.message}</div>
-        )}
-
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name="description"
           control={control}
@@ -51,13 +55,11 @@ const NewIssuePage = () => {
             />
           )}
         />
-        {errors.description && (
-          <div className="text-red-500 text-sm">
-            {errors.description.message}
-          </div>
-        )}
-
-        <Button type="submit">Submit New Issue</Button>
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button type="submit" disabled={isSubmitting}>
+          Submit New Issue
+          {isSubmitting && <Spinner />}
+        </Button>
       </div>
     </form>
   );
